943c27e309ed82dd6ae7750376ade64f80d3fb1f
Mukesh Karn <mukeshkarn@gmail.com>
2015-11-04 14:10:14 +0545

    Updating Month tab

--- sectors/js/monthlyReport.js
+++ sectors/js/monthlyReport.js
@@ -9,6 +9,8 @@ function GetUrlValue(VarSearch){
     }
 	
 }
-var monthNames = [ "January", "February", "March", "April", "May", "June",
+var monthNamesFull = [ "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December" ];
 
+var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
